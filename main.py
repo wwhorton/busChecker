638d7a70f4fad7d1e217bf826c33ca5f8d58cc7a
William Whorton <wwhorton@protonmail.com>
2023-02-25 14:45:59 -0500

    Runs unless terminated, checks hourly, etc.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # Main file for busChecker app
 from bs4 import BeautifulSoup
+import time
 import requests
 import yagmail
+from datetime import datetime
+import atexit
+from signal import signal, SIGINT, SIGTERM, SIGKILL
 
 
 def main():
@@ -9,24 +13,52 @@ def main():
     bus_schedule_url = "https://busstops.aacps.org/public/BusRouteIssues.aspx"
     page = requests.get(bus_schedule_url)
     scraped_data = BeautifulSoup(page.text, 'html.parser')
+    email_sent = False
+    time_format = "%H:%M"
+    current_time = time.localtime()
+    current_time_formatted = time.strftime(time_format)
+    start_time = time.strptime("07:00", time_format)
+    end_time = time.strptime("14:00", time_format)
 
-    if scraped_data.find(bus_number):
-        notify("Eliza's bus isn't running.")
+    send_start_message(current_time_formatted)
 
-    else:
-        notify("Eliza's bus is running.")
+    while True:
+        if datetime.today().weekday() < 5:
+            while start_time < current_time < end_time:
+                if current_time.tm_min is 00 and email_sent is False:
+                    email_sent = check_bus(bus_number, scraped_data)
 
 
-def notify(status):
+def notify(subject, status):
     oath2_source = "./client_secret_1097203586936-1spn72t38cco4qf3mmm5vbl3d1haj4o8.apps.googleusercontent.com.json"
-    receivers = ["whorton@gmail.com", "agreenwhorton@gmail.com"]
+    receivers = ["whorton@gmail.com"]
 
     yag = yagmail.SMTP("whorton@gmail.com", oauth2_file=oath2_source)
     yag.send(
         to=receivers,
-        subject="Schoolbus Disruption",
+        subject=subject,
         contents=status
     )
+
+
+def send_start_message(msg_time):
+    notify("Bus Checker is running", "Bus Checker just started at " + msg_time)
+
+
+@atexit.register
+def send_exit_message():
+    notify("Bus Checker exited", "Bus Checker exited at " + time.localtime().strftime("%H:%M"))
+
+
+signal(SIGINT, send_exit_message)
+signal(SIGTERM, send_exit_message)
+signal(SIGKILL, send_exit_message)
+
+
+def check_bus(bus_number, scraped_data):
+    if bus_number in scraped_data is True:
+        notify("Schoolbus Disruption", "Eliza's bus isn't running.")
+        return True
 
 
 if __name__ == "__main__":
